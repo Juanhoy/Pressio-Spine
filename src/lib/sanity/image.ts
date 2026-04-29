81d63fcd9721a4f9aafdbccb9df150b9e4ce0d8e
Juanjo <juanjo@pressiospine.com>
2026-04-28 23:34:35 -0500

    fix: hardcode sanity id in image helper for netlify reliability

--- src/lib/sanity/image.ts
+++ src/lib/sanity/image.ts
@@ -5,7 +5,7 @@ import { sanityClient } from "./client";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function urlFor(source: any) {
   return createImageUrlBuilder({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "416agrv8",
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+    projectId: "416agrv8",
+    dataset: "production",
   }).image(source);
 }
